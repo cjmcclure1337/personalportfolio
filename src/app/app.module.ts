b7c46718d72b559c4e7c822864b3bddaa9002167
CJMcClure <cjmcclure1337@gmail.com>
2022-03-04 12:42:48 -0600

    Angular app with design template

---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { ProgressbarModule } from "ngx-bootstrap/progressbar";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { PaginationModule } from "ngx-bootstrap/pagination";
+import { AlertModule } from "ngx-bootstrap/alert";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { JwBootstrapSwitchNg2Module } from "jw-bootstrap-switch-ng2";
+import { PopoverModule } from "ngx-bootstrap/popover";
+
+
+import { PagesModule } from "./pages/pages.module";
+
+import { IndexComponent } from "./pages/index/index.component";
+import { ProfilepageComponent } from "./pages/examples/profilepage/profilepage.component";
+import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
+import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
+import { HomeComponent } from './home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    // IndexComponent,
+    // ProfilepageComponent,
+    // RegisterpageComponent,
+    // LandingpageComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    TooltipModule.forRoot(),
+    CollapseModule.forRoot(),
+    TabsModule.forRoot(),
+    PopoverModule.forRoot(),
+    PagesModule,
+    PaginationModule.forRoot(),
+    AlertModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    CarouselModule.forRoot(),
+    ModalModule.forRoot(),
+    JwBootstrapSwitchNg2Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
